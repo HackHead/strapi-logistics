1e0dba52c6f52d5547beef3e66b8f4d8a325b593
Andrew <SolomonAndreew@tutanota.com>
2023-05-05 12:18:51 +0300

    feat: create default layout with header and footer

---
+++ client/src/components/layouts/default.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import Footer from "../organisms/footer";
+import Header from "../organisms/header";
+
+const DefaultLayout = ({children}: {children: ReactNode}) => {
+    return (
+        <>
+        <div className="container-xxl bg-white p-0">
+            <div className="container-xxl position-relative p-0"> 
+                <Header/>
+                {children}
+                <a href="#" className="btn btn-lg btn-secondary btn-lg-square back-to-top">
+                    <i className="bi bi-arrow-up" />
+                </a>
+                <Footer/>
+            </div>
+        </div>
+           
+        </>
+    )
+}
+
+export default DefaultLayout;
