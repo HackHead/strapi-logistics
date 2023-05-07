e24a0d5e11ac5bb554905b13271cbcb3bc7fef58
Andrew <SolomonAndreew@tutanota.com>
2023-05-07 17:44:45 +0300

    feat: config strapi install required plugins add following collections: Page, Contact, Footer

---
+++ strapi-app/config/plugins.js
@@ -0,0 +1,14 @@
+// ./config/plugins.js`
+'use strict';
+
+module.exports = {
+  menus: {
+    config: {
+      layouts: {
+        menuItem: { // This is the menu item edit panel.
+          link: [],
+        },
+      },
+    },
+  },
+};
