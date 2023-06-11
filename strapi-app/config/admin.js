6587f226a290fb97c106e1ca217b43ea7cfa4927
Andrew <SolomonAndreew@tutanota.com>
2023-06-11 12:31:41 +0300

    feat: add strapi admin prefix

--- strapi-app/config/admin.js
+++ strapi-app/config/admin.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+  url: env("PUBLIC_ADMIN_URL", "/straps"),
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
